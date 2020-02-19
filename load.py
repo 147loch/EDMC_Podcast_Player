703c849e7e70f731217ab264c035eb9f2129d330
Ben <ben.woodward@infor.com>
2020-02-19 21:53:58 +0000

    yay it checked in

--- load.py
+++ load.py
@@ -1,9 +1,9 @@
 import listparser
 import feedparser
-
 import urllib.request
-from time import sleep
+import os.path
 
+from pathlib import Path
 from player import player
 
 
@@ -12,13 +12,18 @@ def plugin_start(plugin_dir):
     Load this plugin into EDMC
     """
     print("I am loaded! My plugin folder is {}".format(plugin_dir.encode("utf-8")))
-    available_podcasts = load_opml()
-    print(available_podcasts)
+    #available_podcasts = load_opml()
+    #print(available_podcasts)
 
-    #rss_feed_url = 'http://laveradio.com/feed/laveradio.xml'
-    save_file = 'downloads/FISH Lave Radio Spot.mp3'
-    #download_audio(rss_feed_url, 0, save_file)
-    play_audio(save_file)
+    rss_feed_url = 'http://laveradio.com/feed/laveradio.xml'
+    rss_feed = feedparser.parse(rss_feed_url)
+    explore_rss_feed(rss_feed)
+
+    rss_feed_item_url = get_rss_feed_item_url(rss_feed, 0)
+    #download_audio(rss_feed_item_url)
+    #save_file = 'downloads/lr0.mp3'
+    #download_audio(rss_feed_item_url, 0, save_file)
+    #play_audio(save_file)
     return "Test"
 
 
@@ -29,7 +34,6 @@ def load_opml():
     opml_dict = {}
     for opml_feed in opml_feeds:
         print("adding " + opml_feed.title + " : " + opml_feed.url)
-        #dict1 = {opml_feed.title:'geeks', 'key4':'is', 'key5':'fabulous'}
         opml_dict.update({opml_feed.title : opml_feed.url})
     return opml_dict
 
@@ -37,20 +41,29 @@ def play_audio(file):
     player.start_next_song(file)
     print(file + ' finished')
 
-
-def download_audio(rss_feed_url, index, save_file_name):
-    rss_feed = feedparser.parse(rss_feed_url)
+def get_rss_feed_item_url(rss_feed, index):
     entries = rss_feed.entries
-    first_entry = entries[index]
-    links = first_entry.links
+    entry = entries[index]
+    links = entry.links
     url =''
     for link in links:
         type = link.type
-        #print(type)
         if "audio" in type:
             url = link.href
-            print(url)
-            urllib.request.urlretrieve(url, save_file_name)
+            return url
+    return None
+
+def download_audio(rss_feed_item_url):
+    finename_index = rss_feed_item_url.rfind('/')
+    file_name = "downloads/" + rss_feed_item_url[finename_index+1:]
+    download_file = Path(file_name)
+    if (not(download_file.exists())):
+        print("Downloading " + rss_feed_item_url)
+        getsize = os.path.getsize(file_name)
+        urllib.request.urlretrieve(rss_feed_item_url, file_name)
+    else:
+        print(rss_feed_item_url + " already exists")
+
 
 
 def explore_opml_feed(opml_feed):
@@ -61,8 +74,8 @@ def explore_opml_feed(opml_feed):
     #explore_rss_feed(rss_feed)
 
 def explore_rss_feed(rss_feed):
-    #print(rss_feed)
-    #print(rss_feed.feed.content)
+    print(rss_feed)
+    print(rss_feed.feed.content)
 
     entries = rss_feed.entries
     #print(entries)
